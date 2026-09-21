3ffa687be03ff5af2e5980c9688494a2b7bc3296
Mateus Cançado <mateuscancado.dev@gmail.com>
2026-09-21 11:24:20 -0300

    test(controller): add unit tests for insert endpoint

--- src/test/java/com/mateuscancado/employee_attendance_history/controller/AttendanceControllerTest.java
+++ src/test/java/com/mateuscancado/employee_attendance_history/controller/AttendanceControllerTest.java
@@ -1,6 +1,7 @@
 package com.mateuscancado.employee_attendance_history.controller;
 
 import com.mateuscancado.employee_attendance_history.dto.AttendanceDTO;
+import com.mateuscancado.employee_attendance_history.dto.AttendanceRequestDTO;
 import com.mateuscancado.employee_attendance_history.enums.AttendanceStatus;
 import com.mateuscancado.employee_attendance_history.exception.ResourceNotFoundException;
 import com.mateuscancado.employee_attendance_history.service.AttendanceService;
@@ -11,19 +12,23 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AttendanceController.class)
 public class AttendanceControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockitoBean
     private AttendanceService service;
@@ -124,5 +129,61 @@ public class AttendanceControllerTest {
 
         // Verificação
         Mockito.verify(service, Mockito.times(1)).findByEmployeeId(employeeId);
+    }
+
+    @Test
+    void insert_ShouldReturnStatus201AndAttendanceDTO_WhenSuccess() throws Exception {
+        // Cenário
+        AttendanceRequestDTO requestDTO = new AttendanceRequestDTO(
+                100L,
+                LocalDate.now(),
+                "Atendimento request",
+                AttendanceStatus.OPEN
+        );
+        AttendanceDTO responseDTO = new AttendanceDTO(
+                1L,
+                requestDTO.employeeId(),
+                requestDTO.date(),
+                requestDTO.description(),
+                requestDTO.status()
+        );
+
+        Mockito.when(service.insert(requestDTO)).thenReturn(responseDTO);
+
+        // Execução
+        mockMvc.perform(post("/attendances")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestDTO))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.employeeId").value(100L))
+                .andExpect(jsonPath("$.description").value("Atendimento request"))
+                .andExpect(jsonPath("$.status").value(requestDTO.status().getDescription()));
+
+        // Verificação
+        Mockito.verify(service, Mockito.times(1)).insert(requestDTO);
+    }
+
+    @Test
+    void insert_ShouldReturnStatus400_WhenInvalidDTO() throws Exception {
+        // Cenário
+        AttendanceRequestDTO invalidRequestDTO = new AttendanceRequestDTO(
+                null,
+                LocalDate.now(),
+                "A".repeat(61),
+                AttendanceStatus.OPEN
+        );
+
+        // Execução
+        mockMvc.perform(post("/attendances")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(invalidRequestDTO))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        // Verificação
+        Mockito.verifyNoInteractions(service);
     }
 }
